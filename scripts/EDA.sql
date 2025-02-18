ce9c2a46a04948ee74401a3ca1ab11e828fa544b
NikhilS-10 <sharmanikhil2910@gmail.com>
2025-02-18 14:16:16 +0530

    Update EDA.sql

--- scripts/EDA.sql
+++ scripts/EDA.sql
@@ -1,16 +1,39 @@
+/*
+=============================================================
+ SQL EDA Script: Customer, Product, and Sales Insights
+=============================================================
+Script Purpose:
+    This script performs Exploratory Data Analysis (EDA) on customer, product, and sales data. 
+    It includes:
+    - Aggregating sales by product category and customer.
+    - Calculating key metrics like total sales, average price, and sales percentages.
+    - Analyzing top/lowest performing products and customers.
+    - Exploring sales trends, order dates, and customer demographics.
+
+Usage:
+    Run these queries to uncover patterns, trends, and key insights in the sales and customer data.
+
+=============================================================
+*/
+
+
 -- Exploring all objects in database
 select * from INFORMATION_SCHEMA.TABLES
 
+=============================================================
 -- Exploring all columns in database
 select * from INFORMATION_SCHEMA.COLUMNS
 
+=============================================================
 -- Exploring all Countries our customer come from
 select distinct country from gold.dim_customers
 
+=============================================================
 -- Exploring all Categoris we have
 select distinct category , sub_category , product_name from gold.dim_product
 order by 1,2,3
 
+=============================================================
 -- Sales % by Category
 SELECT p.category,
 	   SUM(s.sales) AS total_sales,
@@ -21,7 +44,7 @@ LEFT JOIN gold.dim_product AS p
 GROUP BY p.category
 ORDER BY total_sales DESC
 
-
+=============================================================
 -- Find date of first order and last order
 -- And how many years of orders are available
 select min(order_date) as first_order_date,
@@ -29,35 +52,41 @@ select min(order_date) as first_order_date,
 	   datediff(year,min(order_date),max(order_date)) as order_range_years
 from gold.fact_sales
 
-
+=============================================================
 -- Find youngest and oldest customer
 select DATEDIFF(year, min(birth_date) , getdate()) as oldest_customer,
 DATEDIFF(year, max(birth_date) , getdate()) as youngest_customer
 from gold.dim_customers
 
+=============================================================
 -- Find the total sales
 select sum(sales) as total_sales from gold.fact_sales
 
-
+=============================================================
 -- Find how many items are sold
 select sum(quantity) as total_quantity  from gold.fact_sales
 
+=============================================================
 -- Find average seeling price 
 select avg(sales) as avg_selling_price from gold.fact_sales
 
+=============================================================
 -- Find total number of orders
 select count(distinct order_number) as total_orders from gold.fact_sales
 
+=============================================================
 -- Find total number of products
 select count(product_key) as total_products from gold.dim_product
 
+=============================================================
 -- Find total number of customers
 select count(customer_key) as total_customer from gold.dim_customers
 
+=============================================================
 -- Find number of customer that has placed an order
 select count(distinct customer_key) total_customer from gold.fact_sales
 
-
+=============================================================
 -- Generate a report that shows all the key matrics of buisness
 select 'Total Sales' as Measure_Name, sum(sales) as Measure_Value from gold.fact_sales
 union all
@@ -78,7 +107,7 @@ select 'Total Sales Last Month' AS Measure_Name, SUM(sales) AS Measure_Value fro
 where year(order_date) = (select year(max(order_date)) from gold.fact_sales)
 and month(order_date) = (select month(max(order_date)) from gold.fact_sales)
 
-
+=============================================================
 -- Top 5 Higest Revenue generating Products
 select top 5 
 p.product_name , 
@@ -102,7 +131,7 @@ group by product_name
 )t 
 where rank <=5
 
-
+=============================================================
 -- Top 5 Lowest Revenue generating Products
 select top 5 p.product_name , sum(s.sales) as total_sales from gold.fact_sales as s 
 left join gold.dim_product as p
@@ -110,6 +139,7 @@ on p.product_key = s.product_key
 group by product_name
 order by total_sales
 
+=============================================================
 -- Top 10 Higest Revenue generating Customers
 select * 
 from(
@@ -124,6 +154,7 @@ group by c.first_name , c.last_name,customer_number
 )t 
 where rank <=10
 
+=============================================================
 -- Top 5 Lowest Revenue generating Customers
 select * 
 from(
@@ -137,3 +168,4 @@ on c.customer_key = s.customer_key
 group by c.first_name , c.last_name,customer_number
 )t 
 where rank <=5
+=============================================================
